18b06a544e916400f347a3778f1f990449c5e67f
AlvaroBala <balaalvaro3@gmail.com>
2024-02-19 11:53:01 +0100

    keywords

--- message.php
+++ message.php
@@ -1,23 +1,38 @@
 <?php
-// connecting to database
+// Load spaCy model
+require_once 'vendor/autoload.php';
+use Spacy\Nlp;
+
+$nlp = new Nlp();
+$nlp->load('en_core_web_sm');
+
+// Connecting to the database
 $conn = mysqli_connect("localhost", "root", "", "bot") or die("Database Error");
 
-// getting user message through ajax
+// Getting user message through AJAX and sanitizing input
 $getMesg = mysqli_real_escape_string($conn, $_POST['text']);
 
-//checking user query to database query
-$check_data = "SELECT replies FROM chatbot WHERE queries LIKE '%$getMesg%'";
-$run_query = mysqli_query($conn, $check_data) or die("Error");
+// Preprocess user input using spaCy
+$doc = $nlp->process($getMesg);
 
-// if user query matched to database query we'll show the reply otherwise it go to else statement
-if(mysqli_num_rows($run_query) > 0){
-    //fetching replay from the database according to the user query
-    $fetch_data = mysqli_fetch_assoc($run_query);
-    //storing replay to a varible which we'll send to ajax
-    $replay = $fetch_data['replies'];
-    echo $replay;
-}else{
-    echo "Sorry can't be able to understand you!";
+// Extract normalized text from spaCy document
+$processedText = $doc->text;
+
+// Query to fetch bot response based on keyword matching
+$query = "SELECT replies FROM chatbot WHERE '$processedText' LIKE CONCAT('%', keywords, '%')";
+$result = mysqli_query($conn, $query);
+
+// If match found based on keywords, send the response back to the user; otherwise, send a default message
+if(mysqli_num_rows($result) > 0){
+    // Fetching reply from the database based on keyword match
+    $fetch_data = mysqli_fetch_assoc($result);
+    // Storing reply to a variable which we'll send to AJAX
+    $reply = $fetch_data['replies'];
+    echo $reply;
+} else {
+    echo "Sorry, I didn't understand your query!";
 }
 
+// Closing the database connection
+mysqli_close($conn);
 ?>
